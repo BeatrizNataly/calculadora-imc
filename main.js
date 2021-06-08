ebd09ddc93a4b6020421da4b0f32043bcbe907cb
Natyce <beatriznatyce@gmail.com>
2021-06-08 15:47:55 -0300

    Atualização da interface e mensagens de resultado do programa.

--- main.js
+++ main.js
@@ -11,22 +11,24 @@ function imc() {
 
         let classif = "";
         if(valorIMC < 6){
-            classif = "com dados mau inseridos. Confira o peso e altura declarados e tente novamente.";
+            classif = " extremamente abaixo do peso. Esta condição é extremamente perigosa para a saúde e requer acompanhamento médico urgente. Caso os dados inseridos estejam incorretos, tente corrigir e calcular o IMC novamente.";
         } else if (valorIMC < 18.5) {
-            classif = "abaixo do peso.";
+            classif = "abaixo do peso. É necessário ingerir mais algumas calorias para atingir o peso ideal.";
         } else if (valorIMC < 25) {
-            classif = "no peso ideal.";
+            classif = "no peso ideal. Muito bem, continue assim!";
         } else if (valorIMC < 30) {
-            classif = "levemente acima do peso.";
+            classif = "levemente acima do peso. Fique atento com sua alimentação e prática física.";
         } else if (valorIMC < 35) {
-            classif = "obesidade grau 1";
+            classif = "obesidade grau 1. Esta condição pode ser prejudicial, recomenda-se mudanças na alimentação e prática física com acompanhamento nutricional.";
         } else if (valorIMC < 40) {
-            classif = "obesidade grau 2";
+            classif = "obesidade grau 2. Esta condição pode ser prejudicial, recomenda-se fortemente acompanhamento médico e nutricional para esta condição.";
+        } else if (valorIMC < 50){
+            classif = "obesidade grau 3. Esta condição pode ser muito prejudicial, recomenda-se fortemente acompanhamento médico e nutricional para esta condição.";
         } else {
-            classif = "obesidade grau 3.";
+            classif = "acima de obesidade grau 3. Esta condição é extremamente perigosa para a saúde e requer acompanhamento médico urgente. Caso os dados inseridos estejam incorretos, tente corrigir e calcular o IMC novamente."
         }
 
-        resultado.textContent = `O IMC é ${valorIMC} e nesta condição está ${classif}`
+        resultado.textContent = `O IMC é ${valorIMC} e você está ${classif}`
 
     } else {
         resultado.textContent = 'Preencha todos os campos para calcular seu IMC.'
